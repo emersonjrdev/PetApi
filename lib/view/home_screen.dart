2ae222c655ce1fb9f59a802c9de1b3d6aebafcd8
EmersonJr <152315731+emersonjrdev@users.noreply.github.com>
2024-11-27 13:40:05 -0300

    subindo projeto

---
+++ lib/view/home_screen.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:pet_adopt/view/cadastro_screen.dart';
+import 'package:pet_adopt/view/login_screen.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Align(
+                alignment: Alignment.topRight,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => const LoginScreen()));
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.transparent,
+                    shadowColor: Colors.transparent,
+                    elevation: 0,
+                  ),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        "Pular",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                        ),
+                      ),
+                      SizedBox(width: 5),
+                      Icon(
+                        Icons.chevron_right,
+                        color: Colors.black,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Image.asset(
+                "assets/images/patinha.png",
+                height: 250,
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                "PetAdopt",
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+              const SizedBox(height: 10),
+              const Spacer(),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => const CadastroScreen()));
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.black,
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 50,
+                    vertical: 20,
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                ),
+                child: const Text(
+                  "Iniciar",
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 30),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/view/pets_screen.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/material.dart';
+import 'package:pet_adopt/controller/pet_controller.dart';
+import 'package:pet_adopt/view/add_pet_screen.dart';
+import 'package:pet_adopt/view/profile_screen.dart';
+import 'package:pet_adopt/widgets/pet_card_screen.dart';
+import 'package:pet_adopt/model/pet_model.dart';
+import 'package:pet_adopt/view/home_screen.dart'; // Certifique-se de que HomeScreen seja sua tela inicial
+
+class PetsScreen extends StatefulWidget {
+  const PetsScreen({super.key});
+
+  @override
+  _PetsScreenState createState() => _PetsScreenState();
+}
+
+class _PetsScreenState extends State<PetsScreen> {
+  late Future<List<PetModel>> pets;
+
+  @override
+  void initState() {
+    super.initState();
+    pets = PetController().fetchPets();
+  }
+
+  // Função de logout
+  void _logout() {
+    Navigator.pushAndRemoveUntil(
+      context,
+      MaterialPageRoute(
+          builder: (context) =>
+              HomeScreen()), // Altere HomeScreen para sua tela inicial
+      (Route<dynamic> route) =>
+          false, // Remove todas as rotas da pilha de navegação
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Pets"),
+          centerTitle: true,
+          automaticallyImplyLeading: false,
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.exit_to_app), // Ícone de saída
+              onPressed: _logout, // Chama a função de logout
+            ),
+          ],
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => AddPetScreen()));
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.grey,
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 20, vertical: 10),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                    ),
+                    child: const Text(
+                      "Adicionar para adoção",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => ProfileScreen()));
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.grey,
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 20, vertical: 10),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                    ),
+                    child: const Row(
+                      children: [
+                        Text(
+                          "Meus Pets",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        SizedBox(width: 5),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+              const SizedBox(height: 20),
+              Expanded(
+                child: FutureBuilder<List<PetModel>>(
+                  future: pets,
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const Center(child: CircularProgressIndicator());
+                    } else if (snapshot.hasError) {
+                      return Center(child: Text('Erro: ${snapshot.error}'));
+                    } else if (snapshot.hasData) {
+                      List<PetModel> petList = snapshot.data!;
+                      return GridView.builder(
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                          crossAxisSpacing: 5,
+                          mainAxisSpacing: 5,
+                          childAspectRatio: 0.785,
+                        ),
+                        itemCount: petList.length,
+                        itemBuilder: (context, index) {
+                          return PetCardScreen(dog: petList[index]);
+                        },
+                      );
+                    } else {
+                      return const Center(
+                          child: Text('Nenhum pet encontrado.'));
+                    }
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
